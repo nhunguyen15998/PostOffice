7f03047fadfaa7152f723a5a62a6fc2d7b7eab10
nhunguyen15998 <aishiteru1509@gmail.com>
2022-04-18 00:53:13 +0700

    Merge Admin

---
+++ Models/ProductAttributeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace post_office.Models
+{
+    public class ProductAttributeModel
+    {
+        public int id { get; set; }
+        public int productId { get; set; }
+        public int? colorID { get; set; }
+        public int? sizeID { get; set; }
+        public int? widthID { get; set; }
+        public int? heightID { get; set; }
+        public int qty { get; set; }
+        public decimal price { get; set; }
+        public DateTime createAt { get; set; }
+
+     
+
+
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace post_office.Models
+{
+    public class UserModel
+    {
+        public static Dictionary<int, string> ls_sts = new Dictionary<int, string>() { { 1, "Activated" }, { 2, "Deactivated" }, { 3, "status 3" }, { 4, "status 4" } };
+
+        [Key]
+        public int id { get; set; }
+
+        //code
+        public string code { get; set; }
+
+        //name
+        [Required(ErrorMessage = "* required")]
+        public string fullName { get; set; }
+        //phone
+        [Required(ErrorMessage = "* required")]
+        [RegularExpression(@"^(0[3|5|7|8|9])+([0-9]{8})$", ErrorMessage = "Invalid phone number")]
+        public string phone { get; set; }
+
+
+        //password
+        [Required(ErrorMessage = "* required")]
+        [MinLength(6, ErrorMessage = "Field password needs a minimum of 6 characters")]
+        public string password { get; set; }
+
+        //role id
+        public int roleId { get; set; } 
+        
+        //branch id
+        public int branchId { get; set; }
+
+        //created at
+        public DateTime createdAt { get; set; }
+
+        //status
+        public int status { get; set; } 
+        
+        
+    }
+}
