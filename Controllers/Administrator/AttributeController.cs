577403e6fe573f32193cbab389d7c9d1a1ec01cc
nhunguyen15998 <aishiteru1509@gmail.com>
2022-04-18 10:07:22 +0700

    add admin folders

---
+++ Controllers/Administrator/AttributeController.cs
@@ -0,0 +1,19 @@
+﻿using post_office.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace post_office.Controllers.Administrator
+{
+    public class AttributeController : Controller
+    {
+        public static int id = 0;
+       
+        public void AttributeCU(AttributeModel model)
+        {
+            
+        }
+    }
+}
