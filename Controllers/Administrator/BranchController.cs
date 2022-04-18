ef3c484e168821a20b78f0c6353e23727e51e373
nhunguyen15998 <aishiteru1509@gmail.com>
2022-04-18 10:07:22 +0700

    add admin folders

---
+++ Controllers/Administrator/BranchController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace post_office.Controllers.Administrator
+{
+    public class BranchController : Controller
+    {
+        public static int id = 0;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
